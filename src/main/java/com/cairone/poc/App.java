f172b418656a25c5832c9e9c2b43a493d9c226dd
Diego Cairone <diegocairone@gmail.com>
2023-10-27 14:18:37 -0300

    wip connect to mssql

--- src/main/java/com/cairone/poc/App.java
+++ src/main/java/com/cairone/poc/App.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class
+		//DataSourceAutoConfiguration.class
+		//,DataSourceTransactionManagerAutoConfiguration.class
 })
 public class App {
 
--- src/main/java/com/cairone/poc/cfg/DatasourceCfg.java
+++ src/main/java/com/cairone/poc/cfg/DatasourceCfg.java
@@ -5,15 +5,17 @@ import com.azure.core.credential.TokenRequestContext;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
+@Slf4j
+//@Configuration
 public class DatasourceCfg {
 
-    @Bean
+    //@Bean
     public DataSource getDataSource() {
 
         TokenRequestContext ctx = new TokenRequestContext()
@@ -26,7 +28,9 @@ public class DatasourceCfg {
         ds.setAccessToken(accessToken.getToken());
         ds.setDatabaseName("poc-passwordless-db");
         ds.setServerName("dcairone.database.windows.net");
+        ds.setAuthentication("ActiveDirectoryMSI");
 
+        log.info("Access token: {}", accessToken.getToken());
         return ds;
     }
 }
